563e10c63b4cd815befcb46b48b85da8fd7aec1b
Andrew D <adiggs1@student.rccd.edu>
2025-06-01 17:23:51 -0700

    Moved Login info from main.html to header.php

---
+++ IndivisualProjectPhase2/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+    <div>
+            <label for="Uname">User name: </label>
+            <input type="text" id="Uname" name="Uname" placeholder="UserName" required>
+            <label for="Pword">Password: </label>
+            <input type="password" id="Pword" name="Pword" placeholder="Password" required>
+        <button type="submit" form="IDinfo" value="Login">Login</button>
+        <button type="submit" form="IDinfo" value="SignUp">SignUp</button>
+        <p class="button">
+            it was the dots
+        </p>
+        </div>
+<head>
+    
+</head>
+<body>
+    
+</body>
+</html>
