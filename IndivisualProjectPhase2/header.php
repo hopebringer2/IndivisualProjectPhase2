038fe20b7c9aff7ba5b0dcc4e919134f34f1d322
Andrew D <adiggs1@student.rccd.edu>
2025-06-07 10:53:21 -0700

    added search bar with button - leads to "search.php"; linked "style.css"; removed testing message/empty space; changed tag holding <form>s to <header>;

--- IndivisualProjectPhase2/header.php
+++ IndivisualProjectPhase2/header.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html>    
     <head>
-        <title><?php echo isset( $pagebar ) ? $pagebar : "Search" ?></title>
-    </head>
-    <form action="main.html" method="POST" id="IDinfo">
-       <div>
-            <label for="Uname">User name: </label>
-            <input type="text" id="Uname" name="Uname" placeholder="UserName" required>
-            <label for="Pword">Password: </label>
-            <input type="password" id="Pword" name="Pword" placeholder="Password" required>
-        <button type="submit" form="IDinfo" value="Login">Login</button>
-        <button type="submit" form="IDinfo" value="SignUp">SignUp</button>
-        <p class="button">
-            it was the dots
-        </p>
+    <link ref="stylesheet" href="style.css">
+    <title><?php echo isset( $pagebar ) ? $pagebar : "Search" ?></title>
+</head>
+<header>
+    <div class="header">
+        <form action="search.php" method="GET" id="Searchinfo">
+           <div>
+                <input type="text" id="item" name="item" placeholder="Searchbar">
+            <button type="submit" form="Searchinfo" value="Login">Search</button>
+            </div>
+        </form>
+        <form action="main.html" method="POST" id="IDinfo">
+           <div>
+                <label for="Uname">User name: </label>
+                <input type="text" id="Uname" name="Uname" placeholder="UserName" required>
+                <label for="Pword">Password: </label>
+                <input type="password" id="Pword" name="Pword" placeholder="Password" required>
+            <button type="submit" form="IDinfo" value="Login">Login</button>
+            <button type="submit" form="IDinfo" value="SignUp">SignUp</button>
         </div>
     </form>
+    </div>
     
-<head>
-    
-</head>
-<body>
-    
-</body>
+</header>
 </html>
