68ac1157a905c30618e3fdb5b059c2b2404a38ba
Andrew D <adiggs1@student.rccd.edu>
2025-06-09 16:10:10 -0700

    made account file to not figure out how to check if logged in: yet;

---
+++ IndivisualProjectPhase2/account.php
@@ -0,0 +1,12 @@
+<html>
+<form action="main.html" method="POST" id="IDinfo">
+           <div>
+                <label for="Uname">User name: </label>
+                <input type="text" id="Uname" name="Uname" placeholder="UserName" required>
+                <label for="Pword">Password: </label>
+                <input type="password" id="Pword" name="Pword" placeholder="Password" required>
+            <button type="submit" form="IDinfo" value="Login">Login</button>
+            <button type="submit" form="IDinfo" value="SignUp">SignUp</button>
+        </div>
+    </form>
+</html>
