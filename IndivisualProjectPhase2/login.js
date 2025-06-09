cd34eb8f4d65d157edaea52ae1e9cb4707e558c3
Andrew D <adiggs1@student.rccd.edu>
2025-06-09 15:26:12 -0700

    made file to show if logged in or not; syntax is not working right;

---
+++ IndivisualProjectPhase2/login.js
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <link ref="stylesheet" href="style.css"></link>
+    </head>
+    class Login {
+        constructor(form, fields) {
+            this.form = form;
+            
+        }
+    }
+    
+        <form action="main.html" method="POST" id="IDinfo">
+           <div>
+                <label for="Uname">User name: </label>
+                <input type="text" id="Uname" name="Uname" placeholder="UserName" required>
+                <label for="Pword">Password: </label>
+                <input type="password" id="Pword" name="Pword" placeholder="Password" required>
+            <button type="submit" form="IDinfo" value="Login">Login</button>
+            <button type="submit" form="IDinfo" value="SignUp">SignUp</button>
+        </div>
+    </form>
+
+</html>
