e183937e636c9c65f092cb62c362efdeb2523c21
Andrew D <adiggs1@student.rccd.edu>
2025-06-09 16:57:57 -0700

    moved "header.php" to "include" file

---
+++ IndivisualProjectPhase2/include/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>    
+    <head>
+    <link ref="stylesheet" href="style.css">
+    <title><?php echo isset( $pagebar ) ? $pagebar : "Search" ?></title>
+</head>
+<header>
+    <div class="header">
+        <a href="Gadgetron.php" id="Gadgetron">Gadgetron</a>
+        <a href="Gadgetron2002.php">2002</a> |
+        <a href="Gadgetron2004.php">2004</a> |
+        <a href="Gadgetron2007.php">2007</a> |
+        <a href="Megacorp.php">Megacorp</a> |
+        <a href="Go-Comet.php">Go-Comet</a> |
+        <a href="GrummelNet.php">GrummelNet Industries</a>
+        <a href="GrummelNet2007.php">2007</a> |
+        <a href="GrummelNet2008.php">2008</a> |
+        <a href="GrummelNet2009.php">2009</a> |
+        <a href="GrummelNet2012.php">2012</a> |
+        <a href="Zurkon.php">Zurkon Family</a>
+
+    </div>
+    
+</header>
+
+<script src="pullitemlist.js"></script>
+</html>
