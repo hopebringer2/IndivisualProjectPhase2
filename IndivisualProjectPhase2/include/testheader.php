2d179db212e018454653c11653af9af2291ff2a0
Andrew D <adiggs1@student.rccd.edu>
2025-06-10 08:15:18 -0700

    finally got header to change cookie "title"

--- IndivisualProjectPhase2/include/testheader.php
+++ IndivisualProjectPhase2/include/testheader.php
@@ -4,11 +4,11 @@
     <link ref="stylesheet" href="style.css">
 </head>
 <header>
-    <div class="header">
-        <a href="test.php" id="Gadgetron">Gadgetron</a>
-        <a href="test.php">2002</a> |
-        <a href="Gadgetron2004.php">2004</a> |
-        <a href="Gadgetron2007.php">2007</a> |
+    <menu class="header" onload="setTitle();">
+        <a href="test.php" onclick="makeTitle('Gadgetron');">Gadgetron</a>
+        <a href="test.php" onclick="makeTitle('Gadgetron 2002');">2002</a> |
+        <a href="Gadgetron2004.php" onclick="makeTitle('Gadgetron 2004');">2004</a> |
+        <a href="Gadgetron2007.php" onclick="makeTitle('Gadgetron 2007');">2007</a> |
         <a href="Megacorp.php">Megacorp</a> |
         <a href="Go-Comet.php">Go-Comet</a> |
         <a href="GrummelNet.php">GrummelNet Industries</a>
@@ -17,13 +17,10 @@
         <a href="GrummelNet2009.php">2009</a> |
         <a href="GrummelNet2012.php">2012</a> |
         <a href="Zurkon.php">Zurkon Family</a>
+    </menu>
+    <p></p>
 
-    </div>
-    
 </header>
 
 <script src="pullitemlist.js"></script>
-<script>
-    document.title = "This is the new page title";
-</script>
 </html>
