122a4af8771e338e78f1c5304a7c280081be40de
Andrew D <adiggs1@student.rccd.edu>
2025-06-10 08:25:37 -0700

    Been so focus on making cookies work I forgot to commit on each change; Made function makeTitle() for making new titles for each page; Made function setTitle() to change document.title using cookie named "title"; Made function checkcookie() and deleteCookies() are basiclly useless but were made to clear cookies and check what cookies had;

--- IndivisualProjectPhase2/pullitemlist.js
+++ IndivisualProjectPhase2/pullitemlist.js
@@ -1,3 +1,42 @@
-<html>
-    
-</html>
+    function makeTitle(name){
+        let newtitle=name;
+        document.cookie="title="+newtitle+"; expires=Thu, 18 Dec 2026 12:00:00 UTC";
+    }
+
+    function setTitle(){
+        let titlename=getcookie("title");
+        if(titlename!=""){
+            document.title=titlename;
+        }else{
+            document.title="fail";
+        }
+    }
+
+    function getcookie(cname){
+        let name = cname + "=";
+        let decodedCookie =decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i=0;i<ca.length;i++){
+            let c=ca[i];
+            while(c.charAt(0)==' '){
+                c=c.substring(1);
+            }
+            if(c.indexOf(name)==0){
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    function checkcookie(){
+        let titlename=getcookie("titlename");
+        if(titlename!=""){
+            return titlename;
+        }else{
+            return test;
+        }
+    }
+
+    function deleteCookies(){
+
+    }
