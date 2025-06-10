b59e977933072afea674d25173ef7efba06d4263
Andrew D <adiggs1@student.rccd.edu>
2025-06-10 08:23:10 -0700

    I am on the verge of making a dynamic page using header meaning I won't have to change each page to fit

--- IndivisualProjectPhase2/include/testheader.php
+++ IndivisualProjectPhase2/include/testheader.php
@@ -4,19 +4,19 @@
     <link ref="stylesheet" href="style.css">
 </head>
 <header>
-    <menu class="header" onload="setTitle();">
+    <menu class="header" onload="setTitle()">
         <a href="test.php" onclick="makeTitle('Gadgetron');">Gadgetron</a>
         <a href="test.php" onclick="makeTitle('Gadgetron 2002');">2002</a> |
-        <a href="Gadgetron2004.php" onclick="makeTitle('Gadgetron 2004');">2004</a> |
-        <a href="Gadgetron2007.php" onclick="makeTitle('Gadgetron 2007');">2007</a> |
-        <a href="Megacorp.php">Megacorp</a> |
-        <a href="Go-Comet.php">Go-Comet</a> |
-        <a href="GrummelNet.php">GrummelNet Industries</a>
-        <a href="GrummelNet2007.php">2007</a> |
-        <a href="GrummelNet2008.php">2008</a> |
-        <a href="GrummelNet2009.php">2009</a> |
-        <a href="GrummelNet2012.php">2012</a> |
-        <a href="Zurkon.php">Zurkon Family</a>
+        <a href="test.php" onclick="makeTitle('Gadgetron 2004');">2004</a> |
+        <a href="test.php" onclick="makeTitle('Gadgetron 2007');">2007</a> |
+        <a href="test.php" onclick="makeTitle('Megacorp');">Megacorp</a> |
+        <a href="test.php">Go-Comet</a> |
+        <a href="test.php">GrummelNet Industries</a>
+        <a href="test.php">2007</a> |
+        <a href="test.php">2008</a> |
+        <a href="test.php">2009</a> |
+        <a href="test.php">2012</a> |
+        <a href="test.php">Zurkon Family</a>
     </menu>
     <p></p>
 
