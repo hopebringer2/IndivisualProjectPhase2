10916c289f13884c21f612cb227e0907add04af0
Andrew D <adiggs1@student.rccd.edu>
2025-06-10 20:45:19 -0700

    Changed page title to "Gadgetron Network"; fixed include header; added id to table tag;

--- IndivisualProjectPhase2/main.php
+++ IndivisualProjectPhase2/main.php
@@ -1,14 +1,14 @@
 <html lang="en">
     
 <?php 
-$pagebar = "Welcome to the shop!";
-include("header.php");
+$pagebar = "Gadgetron Network";
+include("include/header.php");
 ?>
 <head>
     <link rel="stylesheet" href="style.css">
 </head>
 
-    <table style="width: 53%">
+    <table style="width: 53%" id="thetable">
     <p>hello</p>
     </table>
     <table style="width: 53%">
