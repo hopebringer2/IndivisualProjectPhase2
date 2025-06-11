f84ac5f93009a15009b0260ff82cac8e4be5bfa5
Andrew D <adiggs1@student.rccd.edu>
2025-06-10 20:49:48 -0700

    removed links as this made my program too complicated; to my heartbreak;

--- IndivisualProjectPhase2/include/testheader.php
+++ IndivisualProjectPhase2/include/testheader.php
@@ -4,22 +4,6 @@
     <link ref="stylesheet" href="style.css">
 </head>
 <header>
-    <menu class="header">
-        <a href="main.php" onclick="makeTitle('Gadgetron Network')"></a>
-
-        <a href="test.php" onclick="makeTitle('Gadgetron');">Gadgetron</a>
-        <a href="test.php" onclick="makeTitle('Gadgetron 2002');">2002</a> |
-        <a href="test.php" onclick="makeTitle('Gadgetron 2004');">2004</a> |
-        <a href="test.php" onclick="makeTitle('Gadgetron 2007');">2007</a> |
-        <a href="test.php" onclick="makeTitle('Megacorp');">Megacorp</a> |
-        <a href="test.php" onclick="makeTitle('Go-Comet');">Go-Comet</a> |
-        <a href="test.php" onclick="makeTitle('GrummelNet Industries');">GrummelNet Industries</a>
-        <a href="test.php" onclick="makeTitle('GrummelNet Industries 2007');">2007</a> |
-        <a href="test.php" onclick="makeTitle('GrummelNet Industries 2008');">2008</a> |
-        <a href="test.php" onclick="makeTitle('GrummelNet Industries 2009');">2009</a> |
-        <a href="test.php" onclick="makeTitle('GrummelNet Industries 2012');">2012</a> |
-        <a href="test.php" onclick="makeTitle('Zurkon Family');">Zurkon Family</a>
-    </menu>
     <p></p>
 
 </header>
