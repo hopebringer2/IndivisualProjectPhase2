841d8f3cb269120b8388ee1b946ef44e9dba742c
Andrew D <adiggs1@student.rccd.edu>
2025-06-10 20:47:34 -0700

    got Dynamic page title to work; modified links to lead to this page; changed what the title changed to on link click;

--- IndivisualProjectPhase2/include/testheader.php
+++ IndivisualProjectPhase2/include/testheader.php
@@ -4,19 +4,21 @@
     <link ref="stylesheet" href="style.css">
 </head>
 <header>
-    <menu class="header" onload="setTitle()">
+    <menu class="header">
+        <a href="main.php" onclick="makeTitle('Gadgetron Network')"></a>
+
         <a href="test.php" onclick="makeTitle('Gadgetron');">Gadgetron</a>
         <a href="test.php" onclick="makeTitle('Gadgetron 2002');">2002</a> |
         <a href="test.php" onclick="makeTitle('Gadgetron 2004');">2004</a> |
         <a href="test.php" onclick="makeTitle('Gadgetron 2007');">2007</a> |
         <a href="test.php" onclick="makeTitle('Megacorp');">Megacorp</a> |
-        <a href="test.php">Go-Comet</a> |
-        <a href="test.php">GrummelNet Industries</a>
-        <a href="test.php">2007</a> |
-        <a href="test.php">2008</a> |
-        <a href="test.php">2009</a> |
-        <a href="test.php">2012</a> |
-        <a href="test.php">Zurkon Family</a>
+        <a href="test.php" onclick="makeTitle('Go-Comet');">Go-Comet</a> |
+        <a href="test.php" onclick="makeTitle('GrummelNet Industries');">GrummelNet Industries</a>
+        <a href="test.php" onclick="makeTitle('GrummelNet Industries 2007');">2007</a> |
+        <a href="test.php" onclick="makeTitle('GrummelNet Industries 2008');">2008</a> |
+        <a href="test.php" onclick="makeTitle('GrummelNet Industries 2009');">2009</a> |
+        <a href="test.php" onclick="makeTitle('GrummelNet Industries 2012');">2012</a> |
+        <a href="test.php" onclick="makeTitle('Zurkon Family');">Zurkon Family</a>
     </menu>
     <p></p>
 
@@ -24,13 +26,13 @@
 
 <script src="pullitemlist.js"></script>
 <script>
-    function setTitle(){
+    window.onload=function setTitle(){
         let titlename=getcookie("title");
         if(titlename!=""){
             document.title=titlename;
         }else{
             document.title="fail";
         }
-    }
+    };
 </script>
 </html>
