e559dc100bdcfe1d0cd0a345659b513a3734d8e7
Andrew D <adiggs1@student.rccd.edu>
2025-06-11 19:23:48 -0700

    Added test "Tables"; they ain't tables: no wonder i've been stuck on tables;

--- IndivisualProjectPhase2/main.php
+++ IndivisualProjectPhase2/main.php
@@ -8,11 +8,26 @@ include("include/header.php");
     <link rel="stylesheet" href="style.css">
 </head>
 
-    <table style="width: 53%" id="thetable">
-    <p>hello</p>
-    </table>
-    <table style="width: 53%">
-    <p>two</p>
-    </table>
-    
+    <div class="list">
+        <div class="wepicon">
+            <img src="images/Wrench.jpg">
+            <h2>Omni-Wrench</h2>
+            <div class="price">10,000 Bolts</div>
+            <button class="addCart">
+                Add to Cart
+            </button>
+        </div>
+    </div>
+    <div class="list">
+        <div class="wepicon">
+            <img src="images/Apocalypse_Glove_from_RA_screen.png">
+            <h2>Bomb-Glove</h2>
+            <div class="price">15,000 Bolts</div>
+            <button class="addCart">
+                Add to Cart
+            </button>
+        </div>
+    </div>
+
+<script src="pullitemlist.js"></script>
 </html>
