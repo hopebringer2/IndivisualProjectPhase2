ba9d23486beb13af8878e1760401dd6b0caa332b
Andrew D <adiggs1@student.rccd.edu>
2025-06-11 20:37:14 -0700

    readded "testheader.php" to show original version of header;

---
+++ IndivisualProjectPhase2/include/testheader.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>    
+    <head>
+    <link ref="stylesheet" href="style.css">
+</head>
+<header>
+    <p></p>
+
+</header>
+
+<script src="pullitemlist.js"></script>
+</html>
