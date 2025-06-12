f254fed578e395e19556f053a414ed38c67f7f41
Andrew D <adiggs1@student.rccd.edu>
2025-06-11 20:20:42 -0700

    added what could be called exsample of cart page;

--- IndivisualProjectPhase2/page.php
+++ IndivisualProjectPhase2/page.php
@@ -9,7 +9,22 @@
     <div class="checkout">
         <h1>What's in Cart</h1>
         <div class="listCart">
+            <div class="wepicon">
+                <div class="image">
+                    <img src="images/Wrench.jpg">
+                </div>
+                <div class="name">
+                    Omni-Wrench
+                </div>
+                <div class="total">
 
+                </div>
+                <div class="quantity">
+                    <span><</span>
+                    <span>1</span>
+                    <span>></span>
+                </div>
+            </div>
         </div>
         <button class="checkout">Check Out</button>
     </div>
