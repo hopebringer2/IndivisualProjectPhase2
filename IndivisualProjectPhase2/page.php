08840d1decc7ce2d568912bd5e5648811d3820b3
Andrew D <adiggs1@student.rccd.edu>
2025-06-11 19:25:16 -0700

    made "page.php" a checkout page;

---
+++ IndivisualProjectPhase2/page.php
@@ -0,0 +1,16 @@
+<html lang="en">
+
+    <?php 
+        $pagebar = "Gadgetron Network: Cart";
+        include("include/header.php");
+    ?>
+    <head><link rel="stylesheet" href="style.css"></head>
+
+    <div class="checkout">
+        <h1>What's in Cart</h1>
+        <div class="listCart">
+
+        </div>
+        <button class="checkout">Check Out</button>
+    </div>
+</html>
